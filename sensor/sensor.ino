c44e32a61503ab0896ddb077a839d27173b3255d
Pavle-Lakic <pavle.lakic994@gmail.com>
2020-10-08 19:02:19 +0200

    add sensor and AP functionality

---
+++ sensor/sensor.ino
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println();
+  WiFi.begin("It burns when IP", "nakacisenamoj");
+  Serial.print("Connecting !");
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected! IP address = ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop(void)
+{
+  /*
+  unsigned short adc_val;
+  adc_val = analogRead(A0);
+  Serial.print("ADC value = ");
+  Serial.println(adc_val);
+  delay(1000);
+  */
+}
