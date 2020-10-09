c63005f4704ecb97ed56adbf5ac7b0443fe911bb
Pavle-Lakic <pavle.lakic994@gmail.com>
2020-10-09 19:28:42 +0200

    proper simple AP

--- esp8266-12E/esp8266-12E.ino
+++ esp8266-12E/esp8266-12E.ino
@@ -1,73 +1,38 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 
-#ifndef STASSID
-#define STASSID "It burns when IP"
-#define STAPSK  "nakacisenamoj"
+#ifndef APSSID
+#define APSSID "esp base station"
+#define APPSK  "12345678"
 #endif
 
-const char* ssid = STASSID;
-const char* password = STAPSK;
+/* Set these to your desired credentials. */
+const char *ssid = APSSID;
+const char *password = APPSK;
 
 ESP8266WebServer server(80);
 
-const int led = LED_BUILTIN;
-
 void handleRoot() {
-  server.send(200, "text/plain", "nakazo");
+  server.send(200, "text/html", "<h1>You are connected</h1>");
 }
 
-void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
-
-void setup(void) {
+void setup() {
+  delay(1000);
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+  Serial.println();
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
-
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
   server.on("/", handleRoot);
-
-  server.on("/pridev", []() {
-    server.send(200, "text/plain", "odurna");
-  });
-
-  server.onNotFound(handleNotFound);
-
   server.begin();
   Serial.println("HTTP server started");
 }
 
-void loop(void) {
+void loop() {
   server.handleClient();
-  MDNS.update();
 }
